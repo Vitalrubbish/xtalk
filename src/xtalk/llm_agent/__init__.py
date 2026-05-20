c37af86f9d9e112243c0ee853266d8c7b5fa4e4f
xcc-zach <2867389537@qq.com>
2026-05-20 14:20:26 +0800

    refactor: move chat history into llm agent interfaces

--- src/xtalk/llm_agent/__init__.py
+++ src/xtalk/llm_agent/__init__.py
@@ -1,5 +1,5 @@
 from .tools.utils import ToolCallResultArgs, ToolCallResult
-from .interfaces import Agent, AgentContext, AgentOutput
+from .interfaces import Agent, AgentContext, AgentOutput, ChatHistory, PlaybackAIMessageMeta
 from .dummy import DummyAgent
 from .default import DefaultAgent
 from .lts import LTSAgent
@@ -9,10 +9,12 @@ __all__ = [
     "Agent",
     "AgentContext",
     "AgentOutput",
+    "ChatHistory",
     "DummyAgent",
     "DefaultAgent",
     "LTSAgent",
     "ExperimentalAgent",
+    "PlaybackAIMessageMeta",
     "ToolCallResultArgs",
     "ToolCallResult",
 ]
